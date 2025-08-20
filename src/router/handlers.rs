d6cb3edf55a2292d849695239ff87c22bc7c5451
Alex Metelli <alex-metelli@gmx.com>
2025-08-20 16:43:29 +0800

    fixed error ci

--- src/router/handlers.rs
+++ src/router/handlers.rs
@@ -2,19 +2,3 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
-
-/// Internal error wrapper for HTTP handlers.
-///
-/// This struct is used internally for error handling and is not part of the public API.
-#[doc(hidden)]
-pub struct Error(eyre::Error);
-
-impl IntoResponse for Error {
-    fn into_response(self) -> Response {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Error: {}", self.0),
-        )
-            .into_response()
-    }
-}
